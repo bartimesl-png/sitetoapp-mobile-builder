d663d4eb1949f764cf61cd154ce6c885c34e58ee
bartimesl-png <bartimesl@gmail.com>
2026-09-08 13:00:11 +0200

    Add capacitor.config.ts configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,34 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const appId =
+  process.env.APP_PACKAGE_ID ||
+  'com.sitetoapp.test';
+
+const appName =
+  process.env.APP_NAME ||
+  'SiteToApp Test';
+
+const websiteUrl =
+  process.env.APP_WEBSITE_URL ||
+  'https://www.partenairefoyer.com';
+
+const config: CapacitorConfig = {
+  appId,
+
+  appName,
+
+  webDir: 'www',
+
+  server: {
+    url: websiteUrl,
+    cleartext: false,
+    androidScheme: 'https',
+    allowNavigation: ['*'],
+  },
+
+  android: {
+    allowMixedContent: false,
+  },
+};
+
+export default config;
