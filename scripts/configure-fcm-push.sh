497fb5a97089a8b593e72cc32a88c959ee06ed91
bartimesl-png <bartimesl@gmail.com>
2026-09-10 14:59:27 +0200

    Fix formatting in onResume method

--- scripts/configure-fcm-push.sh
+++ scripts/configure-fcm-push.sh
@@ -142,7 +142,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    public void onResume()
+  public void onResume() {
         super.onResume();
 
         String token = getSharedPreferences(
