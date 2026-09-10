d92cb1d5144e8d9b0a96d35e03dcffb1b7b92949
bartimesl-png <bartimesl@gmail.com>
2026-09-10 14:43:56 +0200

    Change onResume method to public in configure-fcm-push.sh

--- scripts/configure-fcm-push.sh
+++ scripts/configure-fcm-push.sh
@@ -142,7 +142,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume()
         super.onResume();
 
         String token = getSharedPreferences(
